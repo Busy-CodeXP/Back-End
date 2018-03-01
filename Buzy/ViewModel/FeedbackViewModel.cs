199c430fd0ae68e6eb6ec5e9f01d2b28be85cea3
Daniell Clemente <clemente2099@gmail.com>
2018-03-01 19:23:36 -0300

    Added new Models

---
+++ Buzy/ViewModel/FeedbackViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Buzy.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Buzy.ViewModel
+{
+    public class FeedbackViewModel
+    {
+        public Assunto assunto { get; set; }
+        public string mensagem { get; set; }
+    }
+}
