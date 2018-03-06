e616811a724f8764e1afa974401d9ea47215ca82
Daniell Clemente <clemente2099@gmail.com>
2018-03-06 18:50:29 -0300

    Changed Migrations

---
+++ Buzy/Migrations/20180305224446_Add-Migration Bus.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Buzy.Migrations
+{
+    public partial class AddMigrationBus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    assunto = table.Column<int>(nullable: false),
+                    mensagem = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_feedbacks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pontosDeOnibus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    latitude = table.Column<double>(nullable: false),
+                    longitude = table.Column<double>(nullable: false),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pontosDeOnibus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    capacidadeSentados = table.Column<int>(nullable: false),
+                    capacisadeEmPe = table.Column<int>(nullable: false),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_veiculos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sensores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    acao = table.Column<int>(nullable: false),
+                    latitude = table.Column<double>(nullable: false),
+                    longitude = table.Column<double>(nullable: false),
+                    tipo = table.Column<int>(nullable: false),
+                    valor = table.Column<int>(nullable: false),
+                    veiculoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sensores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_sensores_veiculos_veiculoId",
+                        column: x => x.veiculoId,
+                        principalTable: "veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "historicoSensores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    data = table.Column<DateTime>(nullable: false),
+                    latitude = table.Column<double>(nullable: false),
+                    longitude = table.Column<double>(nullable: false),
+                    sensorId = table.Column<int>(nullable: true),
+                    valor = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_historicoSensores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_historicoSensores_sensores_sensorId",
+                        column: x => x.sensorId,
+                        principalTable: "sensores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_historicoSensores_sensorId",
+                table: "historicoSensores",
+                column: "sensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sensores_veiculoId",
+                table: "sensores",
+                column: "veiculoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "feedbacks");
+
+            migrationBuilder.DropTable(
+                name: "historicoSensores");
+
+            migrationBuilder.DropTable(
+                name: "pontosDeOnibus");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+
+            migrationBuilder.DropTable(
+                name: "sensores");
+
+            migrationBuilder.DropTable(
+                name: "veiculos");
+        }
+    }
+}
