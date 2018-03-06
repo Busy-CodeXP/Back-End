1df7e359494468e4b135f95a7c9894c476d00920
Daniell Clemente <clemente2099@gmail.com>
2018-03-06 18:51:36 -0300

    Added logic for Dbinitializer to be started

--- Buzy/Program.cs
+++ Buzy/Program.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Buzy.DataAccess;
 
 namespace Buzy
 {
@@ -7,7 +16,39 @@ namespace Buzy
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            bool runSeed = false;
+
+            if (args.Contains("seed"))
+            {
+                runSeed = true;
+                args = args.Where(a => a != "seed").ToArray();
+            }
+
+            var host = BuildWebHost(args);
+
+            if (runSeed) RunSeed(host);
+            else host.Run();
+        }
+
+        private static void RunSeed(IWebHost host)
+        {
+            Console.WriteLine("Running seed...");
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                try
+                {
+                    var context = service.GetRequiredService<BusHelperContext>();
+                    DbInitializer.DbInitialize(context);
+                }
+                catch (Exception e)
+                {
+                    var logger = service.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "Ocorreu um erro enquanto os dados foram enviados");
+                }
+            }
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
