83e8c234e2673792c684e85e24b47616a94a8ee5
Daniell Clemente <clemente2099@gmail.com>
2018-03-12 20:40:33 -0300

    PosicaoViewModel

---
+++ Buzy/ViewModel/PosicaoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buzy.ViewModel
+{
+    public class PosicaoViewModel
+    {
+        public int codigoLinha { get; set; }
+    }
+}
