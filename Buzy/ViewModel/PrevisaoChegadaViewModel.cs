dd778a19e4829449d87ecd826fc6a6c651f7b25c
Daniell Clemente <clemente2099@gmail.com>
2018-03-12 20:40:58 -0300

    PrevisaoChegadaViewModel

---
+++ Buzy/ViewModel/PrevisaoChegadaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buzy.ViewModel
+{
+    public class PrevisaoChegadaViewModel
+    {
+        public int codigoLinha { get; set; }
+    }
+}
