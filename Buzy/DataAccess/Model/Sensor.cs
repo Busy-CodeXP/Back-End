98fbb88447868e1e40a62cb6ec945d37ff9f9b7c
Daniell Clemente <clemente2099@gmail.com>
2018-03-13 21:21:40 -0300

    Retirend proprietes Veivulo, tipo, latitude and longitude

--- Buzy/DataAccess/Model/Sensor.cs
+++ Buzy/DataAccess/Model/Sensor.cs
@@ -8,10 +8,7 @@
 
         public int Id { get; set; }
         public int valor { get; set; }
-        public virtual Veiculo veiculo { get; set; }
         public AcaoSensor acao { get; set; }
-        public TipoSensor tipo { get; set; }
-        public double latitude { get; set; }
-        public double longitude { get; set; }
+        
     }
 }
