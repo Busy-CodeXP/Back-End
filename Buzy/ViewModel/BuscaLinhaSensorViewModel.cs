07ef22ecf7506f61091b9d9a7097a07f96668963
Daniell Clemente <clemente2099@gmail.com>
2018-03-28 21:54:27 -0300

    BuscaLinhaSensorViewModel

---
+++ Buzy/ViewModel/BuscaLinhaSensorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buzy.ViewModel
+{
+    public class BuscaLinhaSensorViewModel
+    {
+        public string codigoLinha { get; set; }
+        public string lotacao { get; set; }
+        public object linha { get; set; }
+    }
+}
