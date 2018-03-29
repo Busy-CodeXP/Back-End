634385cd031a8dc937626ff0301c1e17dc1a4052
Daniell Clemente <clemente2099@gmail.com>
2018-03-28 21:53:03 -0300

    BuscaLinhaResultAdd

---
+++ Buzy/DataAccess/Model/BuscaLinhaResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Buzy.DataAccess.Model
+{
+    [DataContract]
+    public class BuscaLinhaResult
+    {
+        [DataMember(Name = "cl")]
+        public string codigoLinha { get; set; }
+
+        [DataMember(Name = "lc")]
+        public bool isCircular { get; set; }
+
+        [DataMember(Name = "lt")]
+        public string letreiro1 { get; set; }
+
+        [DataMember(Name = "sl")]
+        public int sentido { get; set; }
+
+        [DataMember(Name = "tl")]
+        public int letreiro2 { get; set; }
+
+        [DataMember(Name = "tp")]
+        public string terminalPrincipal { get; set; }
+
+        [DataMember(Name = "ts")]
+        public string terminalSecundario { get; set; }
+
+        [DataMember(Name = "capacidade")]
+        public int capacidade { get; set; }
+
+        [DataMember(Name = "lotacao")]
+        public string lotacao { get; set; }
+    }
+}
